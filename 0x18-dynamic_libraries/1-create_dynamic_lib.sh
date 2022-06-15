a8a9c764381392abf10dda44fa186b277fdbd514
Abayie <jonathanboahene@gmail.com>
2022-06-15 17:41:07 +0000

    [Update]: Edited codes in 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dir=$(dirname $(realpath $0))
-gcc -Wall -pedantic -Werror -Wextra --shared -o $dir/liball.so $dir/*.c 
+gcc -fPIC -c *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o
